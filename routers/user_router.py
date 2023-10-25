af3f45c31718658192a4e723f1e30702e8fa4857
Eugene Elantsev <blinktreeman@gmail.com>
2023-10-26 04:08:47 +0700

    user repository n router done

--- routers/user_router.py
+++ routers/user_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.openapi.models import Response
 from sqlalchemy.orm import Session
 from common.database import SessionLocal
 
@@ -22,8 +23,18 @@ def create(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/users/{user_id}", response_model=User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read(user_id: int, db: Session = Depends(get_db)):
     db_user = user_repository.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.put("/users/{user_id}", response_model=User)
+def update(user: User, db: Session = Depends(get_db)):
+    return user_repository.update_user(db, user)
+
+
+@router.delete("/users/{user_id}", response_model=Response)
+def delete(user_id: int, db: Session = Depends(get_db)):
+    return user_repository.delete_user(db, user_id=user_id)
