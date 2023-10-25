7edcbba6f80b089018af0b6eca0269d02c7af8bb
Eugene Elantsev <blinktreeman@gmail.com>
2023-10-26 03:21:07 +0700

    add route

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
+from common.database import engine
+from common import models
+from routers import user_router
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(user_router.router, tags=["users"])
